1634f60ded09b031b2610b841adc61b6bc743c5c
Rodrigo <rodrigo.sierra@ucsp.edu.pe>
2022-09-29 08:25:49 -0500

    cambio 1.1

---
+++ arrayejem1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int arreglo[] = {21, 56, 98, 15};
+    // arreglo[0] = 10;
+    // arreglo[1] = 5;
+    // arreglo[2] = 4;
+    // arreglo[3] = 8;
+
+    // cout << arreglo[0] << endl;
+    // cout << arreglo[1] << endl;
+    // cout << arreglo[2] << endl;
+    // cout << arreglo[3] << endl;
+
+    for(int i = 0; i < 4; i++){
+        
+        cout << arreglo[i] << endl;
+    }
+}
