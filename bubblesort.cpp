16ec1afe9b390cb24e59f67db0706c67fbdefcde
Rodrigo <rodrigo.sierra@ucsp.edu.pe>
2022-09-30 08:28:34 -0500

    Cambio 1.3

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -2,14 +2,12 @@
 
 using namespace std;
 
-int main() {
-
-    int arregloeglo[] = {21, 65, 98, 15};
-    
-
-    bSort(arregloeglo, 4);
-
-    return 0;
+void imprimirArreglo(int arreglo[], int tam){
+    cout << "[" ;
+    for(int i = 0; i < tam; i++){
+        cout << arreglo[i] << " ";
+    }
+    cout << "]" << endl;
 }
 
 void intercambio(int &a, int &b){
@@ -28,3 +26,36 @@ void bSort(int arreglo[], const int tam){
             }
         }
     }
+}
+
+void inSort(int arreglo[], int n) {
+
+    int i, key, j;
+
+    for (i = 1; i < n; i++){ 
+        key = arreglo[i]; 
+        j = i - 1; 
+  
+        while (j >= 0 && arreglo[j] > key)
+        { 
+            arreglo[j + 1] = arreglo[j]; 
+            j = j - 1; 
+        } 
+        arreglo[j + 1] = key; 
+    } 
+} 
+
+
+int main() {
+
+    int arreglo[] = {21, 65, 98, 15};
+    
+
+    bSort(arreglo, 4);
+    imprimirArreglo(arreglo, 4);
+    inSort(arreglo, 4);
+    imprimirArreglo(arreglo, 4);
+
+    return 0;
+}
+
